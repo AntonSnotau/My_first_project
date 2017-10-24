b5c86db24a38606b122794529715f4decd372f7f
Anton Snotau <snotoff@gmail.com>
2017-10-24 16:04:01 +0200

    added page

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # My_project
+
+Hi,
+this is my first project - portfolio page with video as background.
+Now it's just one page, but I'm planning to add my CV and contacts as linked pages.
+
+Enjoy,
+thank you for attention
+
---
+++ js/app.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const vid = document.getElementById("video-background");
+  const pauseButton = document.querySelector(".myButton");
+
+  // if (window.matchMedia('(prefers-reduced-motion)').matches) {
+  //   vid.removeAttribute("autoplay");
+  //   vid.pause();
+  //   pauseButton.innerHTML = "paused";
+  // }
+
+  function vidFade() {
+    vid.classList.add("stopfade");
+  }
+
+  vid.addEventListener('ended', function() {
+    vid.pause();
+    vidFade();
+  });
+
+  pauseButton.addEventListener("click", function() {
+    vid.classList.toggle("stopfade");
+    if (vid.paused) {
+      vid.play();
+      pauseButton.innerHTML = "pause";
+    } else {
+      vid.pause();
+      pauseButton.innerHTML = "play";
+    }
+  })
+});
